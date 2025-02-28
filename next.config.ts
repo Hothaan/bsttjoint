3718877cb07615d8779b7a43fbd4bafb0b79e9d5
imlinkseo <imlinkseo@gmail.com>
2025-02-28 18:35:54 +0900

    [ADD] 라우팅, 기본 구조 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/bstt",
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
